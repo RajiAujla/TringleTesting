8de82092c82802d4b3c9dcd547b890c4155675c7
RajiAujla <Rajwinderaujla061@gmail.com>
2021-03-03 23:27:35 -0500

    Containing TriangleSolver and main class

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -10,8 +10,62 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
+            bool menuValidate = false,inputValid = false;
+            int side1, side2, side3;
+
             Console.WriteLine("Assignment 2 Solution");
             Console.WriteLine("-----------------------------------------\n");
+            do
+            {
+                Console.WriteLine(" 1. Enter triangle dimensions: \n 2. Exit \n");
+                Console.Write("Please select any option from menu: ");
+                string menu = Console.ReadLine();
+                int menuInteger;
+                bool success = Int32.TryParse(menu, out menuInteger);
+
+                if(success == true)
+                {
+                    if (menuInteger == 1 || menuInteger == 2)
+                    {
+                        menuValidate = true;
+                        if (menuInteger == 1)
+                        {
+                            do
+                            {
+                                Console.Write("\n Enter triangle First Side: ");
+                                string firstSide = Console.ReadLine();
+
+                                Console.Write("\n Enter triangle Second Side: ");
+                                string secondSide = Console.ReadLine();
+
+                                Console.Write("\n Enter triangle Third Side: ");
+                                string thirdSide = Console.ReadLine();
+
+                                bool success1 = Int32.TryParse(firstSide, out side1);
+                                bool success2 = Int32.TryParse(secondSide, out side2);
+                                bool success3 = Int32.TryParse(thirdSide, out side3);
+
+                                if (success1 == true && success2 == true && success3 == true)
+                                {
+                                    inputValid = true;
+                                    string triangleOutput =  TriangleSolver.Analyze(side1, side2, side3);
+                                    Console.WriteLine(triangleOutput);
+                                    Console.ReadLine();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nYou did a mistake!! please enter a integer value");
+                                }
+                            } while (inputValid == false);
+
+                        }
+                        else if (menuInteger == 2)
+                            Environment.Exit(0);
+                    }
+                } else {
+                    Console.WriteLine("\nYou did a mistake!! please enter a integer value for menu");
+                } 
+            } while (menuValidate == false);
         }
-     }
+    } 
 }
---
+++ Assignment2/TriangleSolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public static class TriangleSolver
+    {
+        static string resultedString;
+        public static string Analyze(int side1,int side2,int side3)
+        {
+            if (side1 == side2 && side2 == side3 && side3 == side1)
+            {
+                resultedString = "A. Form a Trianle. \n B. A triangle with all sides equal Known as EQUILATERAL TRIANGLE";
+            } else if (side1 == side2 || side2 == side3 || side3 == side1)
+            {
+                resultedString = "A. Form a Trianle. \n B. A triangle with two sides equal Known as ISOSCELES TRIANGLE";
+            } else if (side1 != side2 && side2 != side3 && side3 != side1)
+            {
+                resultedString = "A. Form a Trianle. \n B. A triangle with All sides Different Known as SCALENE TRIANGLE";
+            } else
+                resultedString = "These three sides donot form a triangle";
+
+            return resultedString;
+        }
+    }
+}
