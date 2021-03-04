779509c9412f4a600598f17dfb945f2154e18580
RajiAujla <Rajwinderaujla061@gmail.com>
2021-03-04 12:04:36 -0500

    Containing the whole project

--- Assignment2/TriangleSolver.cs
+++ Assignment2/TriangleSolver.cs
@@ -11,17 +11,27 @@ namespace Assignment2
         static string resultedString;
         public static string Analyze(int side1,int side2,int side3)
         {
-            if (side1 == side2 && side2 == side3 && side3 == side1)
+            if(side1 >=0 && side2 >= 0 && side3 >= 0)
             {
-                resultedString = "A. Form a Trianle. \n B. A triangle with all sides equal Known as EQUILATERAL TRIANGLE";
-            } else if (side1 == side2 || side2 == side3 || side3 == side1)
+                if (side1 == side2 && side2 == side3 && side3 == side1)
+                {
+                    resultedString = "A. Form a Trianle. \n B. A triangle with all sides equal Known as EQUILATERAL TRIANGLE";
+                }
+                else if (side1 == side2 || side2 == side3 || side3 == side1)
+                {
+                    resultedString = "A. Form a Trianle. \n B. A triangle with two equal sides Known as ISOSCELES TRIANGLE";
+                }
+                else if (side1 != side2 && side2 != side3 && side3 != side1 && (side1 + side2) != side3 && (side2 + side3) != side1 && (side1 + side3) != side1)
+                {
+                    resultedString = "A. Form a Trianle. \n B. A triangle with All sides Different Known as SCALENE TRIANGLE";
+                }
+                else
+                    resultedString = "These three sides donot form a triangle";
+            }
+            else
             {
-                resultedString = "A. Form a Trianle. \n B. A triangle with two sides equal Known as ISOSCELES TRIANGLE";
-            } else if (side1 != side2 && side2 != side3 && side3 != side1)
-            {
-                resultedString = "A. Form a Trianle. \n B. A triangle with All sides Different Known as SCALENE TRIANGLE";
-            } else
-                resultedString = "These three sides donot form a triangle";
+                resultedString = "These three sides donot form a triangle, sides must be a integer";
+             }
 
             return resultedString;
         }
